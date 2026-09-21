fc5101ee7fa0fe1f57a99adbb1f80e49b6aa6c8b
samuellucio7 <samuellucio99999@gmail.com>
2026-09-21 14:38:47 -0300

    imposto de renda

--- 03_estruturas_de_decisao/exercicio_01_imposto_renda.py
+++ 03_estruturas_de_decisao/exercicio_01_imposto_renda.py
@@ -13,3 +13,22 @@ Imprima "Isento" ou o valor total do imposto formatado com 2 casas decimais.
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+salario=float(input('digite seu salário'))
+if 0< salario <=2000:
+    print(f"isento")
+elif 2000<salario <=3000:
+    taxa1=salario*0.08
+    salario_taxa1= salario-taxa1
+    print(f"a sua taxa é R${taxa1: .2f}")
+elif 3000<salario<=4500:
+    taxa1=salario*0.08
+    taxa2=salario*0.18
+    taxa_geral= taxa1+taxa2
+    print(f"a sua taxa é R${taxa_geral: .2f}")
+elif 4500<salario:
+    taxa1=salario*0.08
+    taxa2=salario*0.18
+    taxa3=salario*0.28
+    taxa_geral2=taxa1+taxa2+taxa3
+    print(f"a sua taxa é R${taxa_geral2: .2f}")
+    
